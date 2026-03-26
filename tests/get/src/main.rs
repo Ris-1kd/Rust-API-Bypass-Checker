8d8593a080154a60c16c5a105315abe0b021a842
yunlong <3339199884@qq.com>
2026-03-26 19:26:44 +0800

    add a final end statement in the demo

--- tests/get/src/main.rs
+++ tests/get/src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
-    let mut a = [1, 2, 3, 4, 5];
+    let a = [1, 2, 3, 4, 5];
     let mut i = 0;
     while i < 5 {
-        a[i] = i;
+        let _b = a.get(i);
         i = i + 1;
     }
-    let result = a.get(i);
-    // println!("{}", a[i]);
 }
