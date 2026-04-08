892bbd0c0e5201240ba1468d50f5c9acd2196fb4
Rust-API <3339199884@qq.com>
2026-04-08 21:25:08 +0800

    Update API-counterprats/src/lib.rs

--- API-counterprats/src/lib.rs
+++ API-counterprats/src/lib.rs
@@ -1,13 +1,3 @@
-#![feature(rustc_private)]
-#![feature(box_patterns)]
-extern crate rustc_driver;
-extern crate rustc_interface;
-extern crate rustc_middle;
-extern crate rustc_hir;
-extern crate rustc_data_structures;
-extern crate rustc_session;
-extern crate rustc_span;
-extern crate rustc_abi;
+#![allow(dead_code)]
 
-pub mod callback;
-pub mod options;
+pub fn placeholder() {}
